91fc031ca8418668ff353d05ec75c65801bace64
Timur <Stafr95@mail.ru>
2024-01-09 20:16:19 +0500

    Implementing two-way binding using v-model

---
+++ src/components/PostForm.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-model="post.title"
+      class="input"
+      type="text"
+      placeholder="Название"
+    >
+    <input
+      v-model="post.body"
+      class="input"
+      type="text"
+      placeholder="Описание"
+
+    >
+    <button
+      class="btn"
+    >
+      Создать
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  transition: .6s all ease;
+  &:hover {
+    transition: .5s all ease;
+    background: blue;
+    color: aqua;
+  }
+}
+</style>
