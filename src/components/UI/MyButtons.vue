7f609b2191c1fe4a42ec519f2bd2a58c64f2e239
Timur <Stafr95@mail.ru>
2024-01-09 21:43:14 +0500

    Create Component MyButtons

---
+++ src/components/UI/MyButtons.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="btn">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'my-buttons'
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  transition: .6s all ease;
+  &:hover {
+    transition: .5s all ease;
+    background: blue;
+    color: aqua;
+  }
+}
+</style>
