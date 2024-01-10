151ec023734742ea43e3135a305f39239bf1cbe3
Timur <Stafr95@mail.ru>
2024-01-10 14:48:50 +0500

    Usage v-if and v-show

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
+  <div v-if="posts.length > 0">
     <h3>Список пользователей</h3>
     <post-item
       v-for="post in posts"
       :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
     />
   </div>
+  <h2 v-else style="color: red; text-align: center">
+    Список постов пуст
+  </h2>
 </template>
 
 <script>
 import PostItem from "@/components/PostItem.vue";
 
 export default {
-  components: {
-    PostItem
-  },
+  components: {PostItem},
   props: {
     posts: {
       type: Array,
