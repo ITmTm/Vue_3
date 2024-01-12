1089c81425c17433871e5dfda63a3e0d315cc753
Timur <Stafr95@mail.ru>
2024-01-12 19:49:35 +0500

    Creation component

---
+++ src/pages/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1
+      style="text-align: center"
+    >
+      Данное приложение было создано в качестве TODOшки
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
