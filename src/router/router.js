cea0c9c58f99203bd32b3f8c17bd220a131d4a75
Timur <Stafr95@mail.ru>
2024-01-12 19:51:11 +0500

    Creation router

---
+++ src/router/router.js
@@ -0,0 +1,27 @@
+import Main from "@/pages/Main.vue";
+import {createRouter, createWebHistory} from "vue-router";
+import postPage from "@/pages/PostPage.vue";
+import About from "@/pages/About.vue";
+
+
+const routes = [
+	{
+		path: '/',
+		component: Main
+	},
+	{
+		path: '/post',
+		component: postPage
+	},
+	{
+		path: '/about',
+		component: About
+	}
+]
+
+const router = createRouter({
+	routes,
+	history: createWebHistory(process.env.BASE_URL)
+})
+
+export default router;
