8301bdfd7b1017a58eb03fd29145d262c9b228d4
Timur <Stafr95@mail.ru>
2024-01-15 15:17:12 +0500

    Adding a router to the return button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="navbar">
     <div>Vue 3</div>
     <div class="navbar__btns">
-      <my-buttons @click="$router.push('/')" style="background: darkkhaki">Назад</my-buttons>
+      <my-buttons @click="$router.push('/')" style="background: darkkhaki">На главную</my-buttons>
       <my-buttons @click="$router.push('/post')" style="margin-left: 40px">Посты</my-buttons>
       <my-buttons @click="$router.push('/about')" style="margin-left: 20px">О сайте</my-buttons>
     </div>
