c51af6f2fa14b4f139fad4d0eedb59f1d961bfb0
Timur <Stafr95@mail.ru>
2024-01-15 20:49:45 +0500

    creating a button for Store

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,10 @@
   <div class="navbar">
     <div>Vue 3</div>
     <div class="navbar__btns">
-      <my-buttons @click="$router.push('/')" style="background: darkkhaki">На главную</my-buttons>
+      <my-buttons @click="$router.push('/')" style="background: #5ac2d3">На главную</my-buttons>
       <my-buttons @click="$router.push('/posts')" style="margin-left: 40px">Посты</my-buttons>
       <my-buttons @click="$router.push('/about')" style="margin-left: 20px">О сайте</my-buttons>
+      <my-buttons @click="$router.push('/store')" style="margin-left: 20px; background-color: #b2c3f1">Store</my-buttons>
     </div>
   </div>
 </template>
